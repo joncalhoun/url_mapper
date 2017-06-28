3b9e2e8c8bb3e11941df41e54b347d9b5f46b466
Jon Calhoun <joncalhoun@gmail.com>
2017-06-28 03:55:00 -0400

    Added 3.3

--- main.go
+++ main.go
@@ -29,6 +29,8 @@ var data = `
 {
   "/2.1": "https://gitlab.com/joncalhoun/lenslocked.com/tree/book-2.1-Building_the_server",
   "/3.1": "https://gitlab.com/joncalhoun/lenslocked.com/tree/book-3.1-Routing_with_if_else_statements",
-  "/3.1-diff": "https://gitlab.com/joncalhoun/lenslocked.com/compare/book-2.1-Building_the_server...book-3.1-Routing_with_if_else_statements"
+  "/3.1-diff": "https://gitlab.com/joncalhoun/lenslocked.com/compare/book-2.1-Building_the_server...book-3.1-Routing_with_if_else_statements",
+  "/3.3": "https://gitlab.com/joncalhoun/lenslocked.com/tree/book-3.3-Using_the_gorilla_mux_router",
+  "/3.3-diff": "https://gitlab.com/joncalhoun/lenslocked.com/compare/book-3.1-Routing_with_if_else_statements...book-3.3-Using_the_gorilla_mux_router"
 }
 `
